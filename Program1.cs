96e998c9211094ec3784c9103f85c2bcfaf9d332
JheroNobees <janheroldlegandin36@gmail.com>
2020-12-06 08:38:05 +0800

    Update new lines

--- Program1.cs
+++ Program1.cs
@@ -7,6 +7,8 @@ namespace Portfolio
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
+            Console.WriteLine("How are you?");
             Console.ReadKey();
         }
     }
